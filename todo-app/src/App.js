fa9323f18b77886187d917b3808975f87609ad89
Moisés Santos <moises.santos@farfetch.com>
2019-06-06 14:29:42 +0100

    chore(todo-app): App to stateless

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,10 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 import TodoList from './components/TodoList';
 
-class App extends Component {
-  render() {
-    return <TodoList items={['Turn off the alarm', 'Have breakfast']} />;
-  }
-}
+const App = () => <TodoList items={['Turn off the alarm', 'Have breakfast']} />;
 
 export default App;
