9dcad05496e129883b3aa1ff4b22cb2e9159f3a1
Moisés Santos <moises.santos@farfetch.com>
2019-06-07 16:41:57 +0100

    chore: Removes extra folder

--- calculator-app-v1/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as KeyButton } from './KeyButton';
-export { default as Result } from './Result';
-export { default as KeyPad } from './KeyPad';
-export { default as KeyRow } from './KeyRow';
